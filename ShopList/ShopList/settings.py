cf6ea9c120517c57df46cebe4f4eb8697a29db4b
Unai Pastor Garrido <unaipg@gmail.com>
2016-11-12 23:45:00 +0100

    Añadida la app de restframework y configuración de ficheros estáticos

--- ShopList/ShopList/settings.py
+++ ShopList/ShopList/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
